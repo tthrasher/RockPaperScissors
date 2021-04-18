d74178dbca53c8d45388efcf3ba782e5bd965dc4
tthrasher <tthrasher@gmail.com>
2021-04-18 18:04:22 -0400

    Initial Commit

---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Terry Thrasher on 2021-04-18.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
